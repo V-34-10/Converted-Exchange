3c50df935f2e16ce6082876fab42710eb3acc532
Vlad <vladandryschenko98@gmail.com>
2024-07-17 20:53:12 +0300

    SplashActivity: navigate to MenuActivity, run animation progressBar

--- app/src/main/java/com/exchange/convertedcash/MainActivity.kt
+++ app/src/main/java/com/exchange/convertedcash/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.exchange.convertedcash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnimationUtils
+import com.exchange.convertedcash.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    private val activityScope = CoroutineScope(Dispatchers.Main + SupervisorJob())
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+
+        binding.progressBar.visibility = View.VISIBLE
+        binding.progressBar.startAnimation(
+            AnimationUtils.loadAnimation(
+                this,
+                R.anim.progress_animation
+            )
+        )
+
+        activityScope.launch {
+            delay(3000L)
+            startActivity(Intent(this@MainActivity, MenuActivity::class.java))
+            binding.progressBar.visibility = View.GONE
+            finish()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        activityScope.cancel()
     }
 }

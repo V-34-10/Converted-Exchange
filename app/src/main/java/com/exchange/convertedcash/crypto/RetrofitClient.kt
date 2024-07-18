227af02ce04d7076bc2a83e4399f577c088e0c37
Vlad <vladandryschenko98@gmail.com>
2024-07-18 15:42:52 +0300

    RetrofitClient: use api CryptoCompareService

---
+++ app/src/main/java/com/exchange/convertedcash/crypto/RetrofitClient.kt
@@ -0,0 +1,16 @@
+package com.exchange.convertedcash.crypto
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "https://min-api.cryptocompare.com/"
+
+    val apiService: CryptoCompareService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CryptoCompareService::class.java)
+    }
+}

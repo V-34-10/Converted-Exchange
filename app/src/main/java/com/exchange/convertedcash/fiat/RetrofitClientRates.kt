6cd4e7d428b8f56db95a9c9b02c8068bb37efe6f
Vlad <vladandryschenko98@gmail.com>
2024-07-18 17:28:37 +0300

    RetrofitClientRates: use open exchange rates api

---
+++ app/src/main/java/com/exchange/convertedcash/fiat/RetrofitClientRates.kt
@@ -0,0 +1,16 @@
+package com.exchange.convertedcash.fiat
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClientRates {
+    private const val BASE_URL = "https://openexchangerates.org/api/"
+
+    val apiService: OpenExchangeRatesService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(OpenExchangeRatesService::class.java)
+    }
+}

ea1544bc5c23d481fd575e8b59829f3b64ec88ed
Vlad <vladandryschenko98@gmail.com>
2024-12-15 17:53:03 +0200

    ConverterActivity: use startAnimationProgressBarForResponse, startAnimateClickButton; add initItemSelectedSpinner, onBackPressed; changeImageSelected - add new cryptoLogoCoin

--- app/src/main/java/com/exchange/convertedcash/ui/converter/ConverterActivity.kt
+++ app/src/main/java/com/exchange/convertedcash/ui/converter/ConverterActivity.kt
@@ -1,21 +1,22 @@
 package com.exchange.convertedcash.ui.converter
 
 import android.annotation.SuppressLint
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.view.animation.AnimationUtils
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.exchange.convertedcash.R
-import com.exchange.convertedcash.retrofit.crypto.RetrofitClient
 import com.exchange.convertedcash.databinding.ActivityConverterBinding
-import com.exchange.convertedcash.retrofit.fiat.model.OpenExchangeRatesResponse
+import com.exchange.convertedcash.retrofit.crypto.RetrofitClient
 import com.exchange.convertedcash.retrofit.fiat.RetrofitClientRates
+import com.exchange.convertedcash.retrofit.fiat.model.OpenExchangeRatesResponse
 import com.exchange.convertedcash.ui.menu.MenuCategoryActivity
+import com.exchange.convertedcash.utils.AnimationManager.startAnimateClickButton
+import com.exchange.convertedcash.utils.NavigationManager.navigateToActivity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -25,27 +26,26 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class ConverterActivity : AppCompatActivity() {
-
     private val binding by lazy { ActivityConverterBinding.inflate(layoutInflater) }
+    private val cryptoCurrencies =
+        listOf("BTC", "ETH", "BNB", "PEPE", "SOL", "DOGE", "WIF", "XRP", "TON", "LTC")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-
         fetchAndPopulateCurrencies()
         controlButton()
     }
 
     private fun controlButton() {
-        val animation = AnimationUtils.loadAnimation(this, R.anim.scale_btn)
         binding.convertButton.setOnClickListener {
-            it.startAnimation(animation)
+            startAnimateClickButton(it, this)
             convertCurrency()
         }
         binding.btnBack.setOnClickListener {
-            it.startAnimation(animation)
-            startActivity(Intent(this@ConverterActivity, MenuCategoryActivity::class.java))
-            finish()
+            startAnimateClickButton(it, this)
+            navigateToActivity(MenuCategoryActivity::class.java, this)
         }
         binding.fromCurrencySpinner.onItemSelectedListener =
             object : AdapterView.OnItemSelectedListener {
@@ -54,11 +54,7 @@ class ConverterActivity : AppCompatActivity() {
                     view: View?,
                     position: Int,
                     id: Long
-                ) {
-                    val selectedCurrency = parent.getItemAtPosition(position).toString()
-                    val resourceImage = changeImageSelectedSpinnersItem(selectedCurrency)
-                    binding.fromCurrencyLogo.setBackgroundResource(resourceImage)
-                }
+                ) = initItemSelectedSpinner(parent, position, binding.fromCurrencyLogo)
 
                 override fun onNothingSelected(parent: AdapterView<*>?) {}
             }
@@ -69,14 +65,20 @@ class ConverterActivity : AppCompatActivity() {
                     view: View?,
                     position: Int,
                     id: Long
-                ) {
-                    val selectedCurrency = parent.getItemAtPosition(position).toString()
-                    val resourceImage = changeImageSelectedSpinnersItem(selectedCurrency)
-                    binding.toCurrencyLogo.setBackgroundResource(resourceImage)
-                }
+                ) = initItemSelectedSpinner(parent, position, binding.toCurrencyLogo)
 
                 override fun onNothingSelected(parent: AdapterView<*>?) {}
             }
+    }
+
+    private fun initItemSelectedSpinner(
+        parent: AdapterView<*>,
+        position: Int,
+        currencyLogo: ImageView
+    ) {
+        val selectedCurrency = parent.getItemAtPosition(position).toString()
+        val resourceImage = changeImageSelectedSpinnersItem(selectedCurrency)
+        currencyLogo.setBackgroundResource(resourceImage)
     }
 
     private fun fetchAndPopulateCurrencies() {
@@ -89,16 +91,11 @@ class ConverterActivity : AppCompatActivity() {
             ) {
                 if (response.isSuccessful) {
                     val rates = response.body()?.rates
-
                     val currencyCodes = rates?.keys?.sorted()?.toMutableList() ?: mutableListOf()
-                    val currenciesCrypto =
-                        arrayOf("BTC", "ETH", "BNB", "PEPE", "SOL", "DOGE", "WIF")
-                    currencyCodes.addAll(currenciesCrypto)
+                    currencyCodes.addAll(cryptoCurrencies)
                     initSpinners(currencyCodes)
 
-                } else {
-                    Log.e("ConverterActivity", "Помилка: ${response.code()}")
-                }
+                } else Log.e("ConverterActivity", "Error: ${response.code()}")
             }
 
             override fun onFailure(call: Call<OpenExchangeRatesResponse>, t: Throwable) {
@@ -124,6 +121,9 @@ class ConverterActivity : AppCompatActivity() {
             "PEPE" -> R.drawable.pepe_ico
             "WIF" -> R.drawable.wif_logo
             "DOGE" -> R.drawable.doge_logo
+            "XRP" -> R.drawable.xrp_logo
+            "TON" -> R.drawable.ton_logo
+            "LTC" -> R.drawable.ltc_logo
             else -> R.drawable.usd_logo_circle
         }
         return imageResource
@@ -136,14 +136,14 @@ class ConverterActivity : AppCompatActivity() {
         val toCurrency = binding.toCurrencySpinner.selectedItem.toString()
 
         if (amountString.isEmpty()) {
-            Toast.makeText(this, "Введіть суму", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Enter the amount", Toast.LENGTH_SHORT).show()
             return
         }
 
         val amount = amountString.toDoubleOrNull() ?: 0.0
 
         if (fromCurrency == toCurrency) {
-            Toast.makeText(this, "Конвертація в ту саму валюту", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Conversion to the same currency", Toast.LENGTH_SHORT).show()
             return
         }
 
@@ -161,14 +161,11 @@ class ConverterActivity : AppCompatActivity() {
                         toCurrency
                     )
                     binding.resultTextView.text = resultText
-                } else {
-                    Toast.makeText(
-                        this@ConverterActivity,
-                        "Помилка отримання курсу обміну",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    Log.e("ConverterActivity", "Помилка отримання курсу обміну")
-                }
+                } else Toast.makeText(
+                    this@ConverterActivity,
+                    "Error in receiving the exchange rate",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
     }
@@ -183,10 +180,8 @@ class ConverterActivity : AppCompatActivity() {
         }
     }
 
-    private fun isCryptoCurrency(currency: String): Boolean {
-        val cryptoCurrencies = listOf("BTC", "ETH", "BNB", "PEPE", "SOL", "DOGE", "WIF")
-        return currency.uppercase() in cryptoCurrencies
-    }
+    private fun isCryptoCurrency(currency: String): Boolean =
+        currency.uppercase() in cryptoCurrencies
 
     private fun getCryptoCurrencyRate(fromCurrency: String, toCurrency: String): Double? {
         val call = RetrofitClient.apiService.getCryptocurrencyPrices(
@@ -234,5 +229,11 @@ class ConverterActivity : AppCompatActivity() {
             Log.e("ConverterActivity", "Network Error: ${e.message}")
             null
         }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        super.onBackPressed()
+        navigateToActivity(MenuCategoryActivity::class.java, this)
     }
 }

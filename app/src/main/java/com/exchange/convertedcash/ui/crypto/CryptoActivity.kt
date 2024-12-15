d4e233d9af4b04bf62c1888b3629d003b85df488
Vlad <vladandryschenko98@gmail.com>
2024-12-15 18:40:05 +0200

    CryptoActivity: use animationManager; optimization: callResponseFromCryptoService - add successResponsePrice, initAdapterCrypto,onBackPressed

--- app/src/main/java/com/exchange/convertedcash/ui/crypto/CryptoActivity.kt
+++ app/src/main/java/com/exchange/convertedcash/ui/crypto/CryptoActivity.kt
@@ -5,17 +5,19 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
-import com.exchange.convertedcash.R
 import com.exchange.convertedcash.adapters.CryptocurrencyAdapter
-import com.exchange.convertedcash.retrofit.crypto.RetrofitClient
 import com.exchange.convertedcash.databinding.ActivityMenuBinding
-import com.exchange.convertedcash.retrofit.crypto.model.CryptoCompareResponse
-import com.exchange.convertedcash.retrofit.crypto.model.CryptoListResponse
 import com.exchange.convertedcash.model.Cryptocurrency
+import com.exchange.convertedcash.retrofit.crypto.RetrofitClient
+import com.exchange.convertedcash.retrofit.crypto.model.CryptoCompareResponse
+import com.exchange.convertedcash.retrofit.crypto.model.CryptoInfo
+import com.exchange.convertedcash.retrofit.crypto.model.CryptoListResponse
 import com.exchange.convertedcash.ui.converter.ConverterActivity
 import com.exchange.convertedcash.ui.menu.MenuCategoryActivity
+import com.exchange.convertedcash.utils.AnimationManager.startAnimateClickButton
+import com.exchange.convertedcash.utils.AnimationManager.startAnimationProgressBarForResponse
+import com.exchange.convertedcash.utils.NavigationManager.navigateToActivity
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -29,36 +31,24 @@ class CryptoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-
-        binding.progressBar.visibility = View.VISIBLE
-        binding.progressBar.startAnimation(
-            AnimationUtils.loadAnimation(
-                this,
-                R.anim.progress_animation
-            )
-        )
-
-        initCryptoAdapter()
+        startAnimationProgressBarForResponse(binding.progressBar)
+        callResponseFromCryptoService()
         navigateButton()
     }
 
     private fun navigateButton() {
-        val animation = AnimationUtils.loadAnimation(this, R.anim.scale_btn)
         binding.btnConvert.setOnClickListener {
-            it.startAnimation(animation)
-            startActivity(Intent(this@CryptoActivity, ConverterActivity::class.java))
-            finish()
+            startAnimateClickButton(it, this)
+            navigateToActivity(ConverterActivity::class.java, this)
         }
         binding.btnBack.setOnClickListener {
-            it.startAnimation(animation)
-            startActivity(Intent(this@CryptoActivity, MenuCategoryActivity::class.java))
-            finish()
+            startAnimateClickButton(it, this)
+            navigateToActivity(MenuCategoryActivity::class.java, this)
         }
     }
 
-    private fun initCryptoAdapter() {
-        val cryptoSymbols = "BTC,ETH,BNB,PEPE,SOL,DOGE,WIF,XRP"
-
+    private fun callResponseFromCryptoService() {
+        val cryptoSymbols = "BTC,ETH,BNB,PEPE,SOL,DOGE,WIF,XRP,TON,LTC"
         val listCall = RetrofitClient.apiService.getCryptocurrencyList()
         listCall.enqueue(object : Callback<CryptoListResponse> {
             override fun onResponse(
@@ -67,53 +57,67 @@ class CryptoActivity : AppCompatActivity() {
             ) {
                 if (response.isSuccessful) {
                     val cryptoData = response.body()?.Data ?: emptyMap()
-                    Log.d("MenuActivity", "Список криптовалют: ${response.body()}")
+                    Log.d("CryptoActivity", "List of cryptocurrencies: ${response.body()}")
                     val priceCall =
                         RetrofitClient.apiService.getCryptocurrencyPrices(fromSymbols = cryptoSymbols)
                     priceCall.enqueue(object : Callback<CryptoCompareResponse> {
-                        @SuppressLint("NotifyDataSetChanged")
                         override fun onResponse(
                             call: Call<CryptoCompareResponse>,
                             priceResponse: Response<CryptoCompareResponse>
                         ) {
                             if (priceResponse.isSuccessful) {
-                                val priceData = priceResponse.body()?.RAW
-                                val cryptoList = mutableListOf<Cryptocurrency>()
-                                Log.d("MenuActivity", "Список криптовалют: ${priceResponse.body()}")
-                                priceData?.forEach { (symbol, data) ->
-                                    val usdPrice = data["USD"]?.PRICE
-
-                                    val fullName = cryptoData[symbol]?.FullName ?: symbol
-                                    cryptoList.add(
-                                        Cryptocurrency(
-                                            symbol = symbol,
-                                            name = fullName,
-                                            usdPrice = usdPrice
-                                        )
-                                    )
-                                }
-                                adapter = CryptocurrencyAdapter(cryptoList)
-                                binding.listCurrency.adapter = adapter
-                                adapter.notifyDataSetChanged()
-                                binding.progressBar.visibility = View.GONE
+                                successResponsePrice(priceResponse, cryptoData)
                             } else {
-                                Log.e("MenuActivity", "Помилка цін: ${priceResponse.code()}")
+                                Log.e("CryptoActivity", "Price mistake: ${priceResponse.code()}")
                             }
                         }
 
                         override fun onFailure(call: Call<CryptoCompareResponse>, t: Throwable) {
-                            Log.e("MenuActivity", "Помилка цін: ${t.message}")
+                            Log.e("CryptoActivity", "Price mistake: ${t.message}")
                         }
                     })
                 } else {
-                    Log.e("MenuActivity", "Помилка списку: ${response.code()}")
+                    Log.e("CryptoActivity", "Error List: ${response.code()}")
                 }
             }
 
             override fun onFailure(call: Call<CryptoListResponse>, t: Throwable) {
-                Log.e("MenuActivity", "Помилка списку: ${t.message}")
+                Log.e("CryptoActivity", "Error List: ${t.message}")
             }
         })
     }
 
+    private fun successResponsePrice(
+        priceResponse: Response<CryptoCompareResponse>,
+        cryptoData: Map<String, CryptoInfo>
+    ) {
+        val priceData = priceResponse.body()?.RAW
+        val cryptoList = mutableListOf<Cryptocurrency>()
+        Log.d("CryptoActivity", "List of cryptocurrencies: ${priceResponse.body()}")
+        priceData?.forEach { (symbol, data) ->
+            cryptoList.add(
+                Cryptocurrency(
+                    symbol = symbol,
+                    name = cryptoData[symbol]?.FullName ?: symbol,
+                    usdPrice = data["USD"]?.PRICE
+                )
+            )
+        }
+        initAdapterCrypto(cryptoList)
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun initAdapterCrypto(cryptoList: List<Cryptocurrency>) {
+        adapter = CryptocurrencyAdapter(cryptoList)
+        binding.listCurrency.adapter = adapter
+        adapter.notifyDataSetChanged()
+        binding.progressBar.visibility = View.GONE
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        super.onBackPressed()
+        startActivity(Intent(this@CryptoActivity, MenuCategoryActivity::class.java))
+        finish()
+    }
 }
